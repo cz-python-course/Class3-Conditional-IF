1ec578e5f769d87850786bf0529c4cbaa66d0b43
Fernando Crozetta <fernando@czetta.com>
2019-09-20 10:01:21 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+name = input('What is your name')
+dayOne = input('How many cups of coffee you drank 1 day ago?')
+dayTwo = input('How many cups of coffee you drank 2 days ago?')
+dayThree = input('How many cups of coffee you drank 3 days ago?')
+average = (int(dayOne) + int(dayTwo) + int(dayThree)) / 3
+print('the average is:' + str(average))
